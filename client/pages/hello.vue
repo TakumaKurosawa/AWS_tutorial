d29243fa570daa5e8dea26096b9e4ce0a20a7b8c
Takumaron <tennis.takumax@gmail.com>
2019-12-21 09:39:20 +0900

    serverとのaxios接続を確認

---
+++ client/pages/hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-content>
+    <h1>server response ...</h1>
+    <h2 class="ml-6">{{ msg }}</h2>
+  </v-content>
+</template>
+
+<script>
+export default {
+  async asyncData({ app }) {
+    const res = await app.$axios.get('http://localhost:8080/ping')
+    return {
+      msg: res.data
+    }
+  }
+}
+</script>
