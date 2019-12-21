3f9b20c9dec2dbb6572ef982eb3c5fb99f9ed1ee
Takumaron <tennis.takumax@gmail.com>
2019-12-21 09:05:35 +0900

    apiアプリケーションの雛形を作成

---
+++ server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// connection testAPI
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping",
+		})
+	})
+
+	// ポートを設定しています。
+	r.Run(":3000")
+}
